f77f6a2125e0c0040d3678cedf75cc258ef4d3cb
Sam <sam0000@live.nl>
2021-03-02 15:39:11 +0100

    les1 erbij

---
+++ les1/funties-return/funties-return/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace funties_return
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(ewhduwfjsd());
+        }
+        static string ewhduwfjsd()
+        {
+            string name = "Sam";
+
+            return "Hello, " + name + "!";
+        }
+    }
+}
