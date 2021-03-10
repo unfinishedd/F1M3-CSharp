5f0c2f4b03fba6ba64eaabb3d9a073d45a772c86
Sam <sam0000@live.nl>
2021-03-10 15:48:13 +0100

    les 4 ook af

---
+++ in de les/9-3-2021/ArraysAndLoops/ArraysAndLoops/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ArraysAndLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int getal = 10;
+                            //  0   1   2   3   4   5
+            int[] getallen = { 10, 20, 30, 40, 50, 60 };
+            //2 loops
+            //1: For loop
+            //2: Foreach
+
+
+            int opsomming = 0;
+
+            foreach(int hetGetal in getallen)
+            {
+                //Console.WriteLine(hetGetal);
+                opsomming += hetGetal;
+            }
+
+            Console.WriteLine(opsomming);
+            /*
+            for(int i = 0; i < getallen.Length; i++)
+            {
+                Console.WriteLine(getallen[i]);
+            }
+            */
+
+        }
+    }
+}
--- les2/les2_Var/les2/Program.cs
+++ les2/les2_Var/les2/Program.cs
@@ -6,15 +6,13 @@ namespace les2 //var - > datatypes
     {
         static void Main(string[] args)
         {
-            public static void Main()
-            {
-                int a = 1;
-                float b = 0.5f;
-                double c = 1.32142412441d;
-                string d = "Word";
-                char e = 'c';
-                bool f = true;
-            }
+            int a = 1;
+            float b = 0.5f;
+            double c = 1.32142412441d;
+            string d = "Word";
+            char e = 'c';
+            bool f = true;
+            
         }
     }
 }
